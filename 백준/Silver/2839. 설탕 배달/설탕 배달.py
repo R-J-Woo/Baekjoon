8da3f9b9730892b36dd1df5c1cf7f89f6cca4383
R-J-Woo <68835451+R-J-Woo@users.noreply.github.com>
2024-06-05 00:44:45 +0900

    [Silver IV] Title: 설탕 배달, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,15 +1,20 @@
-kg = int(input())
+sugar = input()
+sugar = int(sugar)
 
 count = 0
-while kg >= 0:
-    # 5로 나눠지는 순간 바로 종료
-    if kg % 5 == 0:
-        count += kg // 5
-        print(count)
-        break
-    else:
-        count += 1
-        kg -= 3
 
+while sugar >= 3:
+     
+    if sugar % 5 == 0: # 남은 설탕의 무게가 5로 나뉘어지면
+        count += sugar // 5
+        sugar %= 5
+        break
+
+    sugar -= 3
+    count += 1
+
+
+if sugar == 0:
+    print(count)
 else:
     print(-1)
