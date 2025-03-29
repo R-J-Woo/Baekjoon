db9e4bb5bfd3171c990b589812e7c808b4f2530d
R-J-Woo <jewoo20000624@gmail.com>
2025-03-30 00:08:34 +0900

    [Silver I] Title: 주지수, Time: 3084 ms, Memory: 130180 KB -BaekjoonHub

---
+++ 백준/Silver/15724. 주지수/주지수.py
@@ -0,0 +1,18 @@
+n, m = map(int, input().split())
+people_arr = [list(map(int, input().split())) for _ in range(n)]
+
+prefix_sum = [[0] * (m + 1) for _ in range(n + 1)]
+
+for i in range(1, n + 1):
+    for j in range(1, m + 1):
+        prefix_sum[i][j] = (prefix_sum[i-1][j] + prefix_sum[i][j-1] - prefix_sum[i-1][j-1] + people_arr[i-1][j-1])
+
+k = int(input())
+
+for _ in range(k):
+    x1, y1, x2, y2 = map(int, input().split())
+    
+    # 부분 합 계산
+    result = (prefix_sum[x2][y2] - prefix_sum[x1-1][y2] - prefix_sum[x2][y1-1] + prefix_sum[x1-1][y1-1])
+    
+    print(result)
