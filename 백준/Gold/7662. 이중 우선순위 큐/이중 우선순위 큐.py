a976586061fbd560faedddd6dc583254c01318df
R-J-Woo <jewoo20000624@gmail.com>
2025-05-03 01:04:52 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 4056 ms, Memory: 435912 KB -BaekjoonHub

---
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.py
@@ -0,0 +1,44 @@
+import heapq
+from collections import defaultdict
+
+t = int(input())
+
+for _ in range(t):
+    k = int(input())
+    min_heap = []
+    max_heap = []
+    visited = defaultdict(bool)
+
+    for i in range(k):
+        cmd, num = input().split()
+        num = int(num)
+
+        if cmd == 'I':
+            heapq.heappush(min_heap, (num, i))
+            heapq.heappush(max_heap, (-num, i))
+            visited[i] = True
+
+        elif cmd == 'D':
+            if num == 1:
+                while max_heap and not visited[max_heap[0][1]]:
+                    heapq.heappop(max_heap)
+                if max_heap:
+                    visited[max_heap[0][1]] = False
+                    heapq.heappop(max_heap)
+            elif num == -1:
+                while min_heap and not visited[min_heap[0][1]]:
+                    heapq.heappop(min_heap)
+                if min_heap:
+                    visited[min_heap[0][1]] = False
+                    heapq.heappop(min_heap)
+
+    # 남은 원소들 정리
+    while min_heap and not visited[min_heap[0][1]]:
+        heapq.heappop(min_heap)
+    while max_heap and not visited[max_heap[0][1]]:
+        heapq.heappop(max_heap)
+
+    if not min_heap or not max_heap:
+        print("EMPTY")
+    else:
+        print(-max_heap[0][0], min_heap[0][0])
