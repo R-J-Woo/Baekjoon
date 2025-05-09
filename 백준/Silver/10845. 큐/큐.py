3ed3ddaadcc7efdaff094b6dbf27d0ded0ef171b
R-J-Woo <jewoo20000624@gmail.com>
2025-05-09 16:24:43 +0900

    Update 큐.py

--- 백준/Silver/10845. 큐/큐.py
+++ 백준/Silver/10845. 큐/큐.py
@@ -1,4 +1,6 @@
-# 입력을 input()으로 받기보다는 sys.stdin.readline으로 받는 것이 더 빠르다
+# 시간 초과(TLE) 가 나는 가장 큰 원인은 input()을 반복문 안에서 너무 많이 호출하기 때문이다
+# 백준처럼 많은 입력이 주어지는 경우, 파이썬의 기본 input()은 느리기 때문에 빠른 입력 방법으로 바꿔주는 것이 좋다
+# 해결 방법 → sys.stdin.readline() 사용
 from collections import deque
 import sys
 
