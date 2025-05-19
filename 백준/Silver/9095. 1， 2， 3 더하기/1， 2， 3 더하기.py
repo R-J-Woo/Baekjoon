3b096a54018887b6629b7118b05d30ab98802b0e
R-J-Woo <jewoo20000624@gmail.com>
2025-05-19 17:28:55 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    dp = [0] * (n + 1)
+    dp[0] = 1
+    dp[1] = 1
+    for i in range(2, n + 1):
+        if i >= 1:
+            dp[i] += dp[i - 1]
+        if i >= 2:
+            dp[i] += dp[i - 2]
+        if i >= 3:
+            dp[i] += dp[i - 3]
+
+    print(dp[n])
