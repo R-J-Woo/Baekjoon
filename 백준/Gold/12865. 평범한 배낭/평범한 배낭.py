3fc7751989d5e971dbf08e2e5869ba71130206dd
R-J-Woo <jewoo20000624@gmail.com>
2025-07-25 12:17:12 +0900

    [Gold V] Title: 평범한 배낭, Time: 2508 ms, Memory: 36024 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -0,0 +1,17 @@
+n, k = map(int, input().split())
+wv = []
+for _ in range(n):
+    wv.append(list(map(int, input().split())))
+
+wv.sort()
+
+k_list = [0] * (k + 1)
+for i in range(n):
+    # 오름차순으로 하면 중복 사용이 있을 수 있기 때문에 내림차순으로 순회
+    for j in range(k, wv[i][0] - 1, -1):
+        if j == wv[i][0]:
+            k_list[j] = wv[i][1]
+        elif k_list[j - wv[i][0]] != 0:
+            k_list[j] = max(k_list[j], k_list[j - wv[i][0]] + wv[i][1])
+
+print(max(k_list[1:]))
