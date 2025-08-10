09a36e0b6fb4ddc9786c037c0b7da98eb2a6d612
R-J-Woo <jewoo20000624@gmail.com>
2025-08-10 15:13:40 +0900

    [Silver V] Title: 너의 평점은, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.py
@@ -0,0 +1,14 @@
+ratings = {'A+': 4.5, 'A0': 4.0, 'B+': 3.5, 'B0': 3.0
+          , 'C+': 2.5, 'C0': 2.0, 'D+': 1.5, 'D0': 1.0, 'F': 0.0}
+total_credit = 0
+total_rating = 0
+for _ in range(20):
+    subject, credit, rating = input().split()
+
+    if rating == 'P':
+        continue
+
+    total_credit += float(credit)
+    total_rating += float(credit) * ratings[rating]
+
+print(round(total_rating / total_credit, 6))
