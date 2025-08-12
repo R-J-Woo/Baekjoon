d35fe25fb83f1edcc08e7f365d827d254043ba71
R-J-Woo <jewoo20000624@gmail.com>
2025-08-12 12:30:18 +0900

    [Gold IV] Title: 문자열 폭발, Time: 856 ms, Memory: 43596 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
@@ -1,3 +1,7 @@
+# 아이디어: 스택을 이용
+# 문장의 글자를 하나하나 추가해 가면서, 문장의 뒷 부분이 폭발 문자열과 같다면 삭제
+# ex) mirkovC4의 뒷 부분이 폭발 문자열 C4와 같으므로 pop 두번 실행
+
 word = input()
 boom = input()
 
@@ -5,6 +9,7 @@ stack = []
 for w in word:
     stack.append(w)
     length = len(boom)
+    # 문장의 뒷 부분이 폭발 문자열과 같다면 폭발 문자열 길이만큼 pop
     if len(stack) >= length and ''.join(stack[-length:]) == boom:
         for _ in range(length):
             stack.pop()
