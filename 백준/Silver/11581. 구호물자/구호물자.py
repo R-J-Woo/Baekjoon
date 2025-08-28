6e2688531ef61b8269192e614dea7f288a1424c9
R-J-Woo <jewoo20000624@gmail.com>
2025-08-28 15:29:39 +0900

    [Silver I] Title: 구호물자, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11581. 구호물자/구호물자.py
+++ 백준/Silver/11581. 구호물자/구호물자.py
@@ -1,3 +1,6 @@
+# 아이디어: 깊이 우선 탐색(DFS) 알고리즘 이용
+# 깊이 우선 탐색을 돌면서 visited를 이용해서 방문한 적 있는 노드에 다시 방문했음을 판단
+
 import sys
 sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
@@ -14,13 +17,13 @@ def dfs(graph, start, visited):
     global cycle_flag
     visited[start] = True
     for i in graph[start]:
-        if visited[i] == True:
+        if visited[i] == True: # 이미 방문한 적이 있는 노드라면 CYCLE
             cycle_flag = True
             return
         else:
             dfs(graph, i, visited)
 
-    visited[start] = False
+    visited[start] = False # 다시 돌아갈 때 방문한 적 없는 상태로 복구
 
 visited = [False] * (n + 1)    
 dfs(graph, 1, visited)
