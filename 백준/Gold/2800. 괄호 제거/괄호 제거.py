6ebd25a16303821da81a763076987b020cab91ea
R-J-Woo <jewoo20000624@gmail.com>
2026-01-15 11:25:26 +0900

    [Gold IV] Title: 괄호 제거, Time: 64 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/2800. 괄호 제거/괄호 제거.py
@@ -0,0 +1,35 @@
+from itertools import combinations
+
+string = input()
+
+# 괄호의 짝을 구하기
+brackets = []
+stack = []
+for i in range(len(string)):
+    if string[i] == '(':
+        stack.append(i)
+    elif string[i] == ')':
+        left = stack.pop()
+        right = i
+        brackets.append([left, right])
+
+# 1~괄호 짝의 개수까지 조합하면서, 출력에서 제외할 괄호의 인덱스 짝을 구함
+total_result = set()
+for count in range(1, len(brackets) + 1):
+    for c in combinations(brackets, count):
+        total_brackets = []
+        for i in range(count):
+            total_brackets.append(c[i][0])
+            total_brackets.append(c[i][1])
+
+        result = ''
+        for i in range(len(string)):
+            if i not in total_brackets:
+                result += string[i]
+
+        total_result.add(result)
+
+# 사전 순으로 정렬
+total_result = list(sorted(total_result))
+for result in total_result:
+    print(result)
