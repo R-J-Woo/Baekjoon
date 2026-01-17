6129f50649eb66bb89cb6def431566c1da9b39a1
R-J-Woo <jewoo20000624@gmail.com>
2026-01-17 10:13:17 +0900

    [Silver V] Title: 국회의원 선거, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1417. 국회의원 선거/국회의원 선거.py
+++ 백준/Silver/1417. 국회의원 선거/국회의원 선거.py
@@ -1,33 +1,20 @@
-# 아이디어: 기호 1번을 제외한 사람들이 받은 표 중 가장 큰 수를 가져와서 1번과 비교한 후
-# 1번의 표보다 크면 1번의 표를 1 늘리고 가장 큰 수의 표를 1줄이는 방식으로 한다
-# 기호 1번을 제외한 사람들의 표 중 가장 큰 수가 기호 1번보다 작으면 종료
-# 우선순위 큐 이용
+N = int(input())
+p = []
+for _ in range(N):
+    p.append(int(input()))
 
-import heapq
-import sys
-input = sys.stdin.readline
+result = 0
+while True:
+    max_idx = 0
+    for i in range(1, len(p)):
+        if p[max_idx] <= p[i]:
+            max_idx = i
 
-n = int(input())
-nums = []
-for _ in range(n):
-    nums.append(int(input()))
+    if max_idx == 0:
+        break
 
-if len(nums) == 1:
-    print(0)
-else:
-    first = nums[0]
-    other = []
-    for i in range(1, len(nums)):
-        heapq.heappush(other, -nums[i])
+    result += 1
+    p[max_idx] -= 1
+    p[0] += 1
 
-    count = 0
-    while True:
-        max_value = -heapq.heappop(other)
-        if max_value >= first:
-            count += 1
-            first += 1
-            max_value -= 1
-            heapq.heappush(other, -max_value)
-        else:
-            print(count)
-            break
+print(result)
