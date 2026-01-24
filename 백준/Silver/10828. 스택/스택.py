fb20535fc1b6450d84b17aaed210f58207d9eeee
R-J-Woo <jewoo20000624@gmail.com>
2026-01-24 10:13:10 +0900

    [Silver IV] Title: 스택, Time: 52 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.py
+++ 백준/Silver/10828. 스택/스택.py
@@ -1,32 +1,28 @@
-
-# idea: 앞선 회의 종료시간보다 늦게 시작하는 회의 중에서, 회의가 가장 일찍 끝나는 회의로 이어간다
-
-iter = input()
+import sys
+input = sys.stdin.readline
 
 stack = []
-command_list = []
-for _ in range(int(iter)):
-    command = input()
-    command_list.append(command)
 
-for command in command_list:
-    if command[:4] == "push":
-        stack.append(int(command[5:]))
-    elif command[:3] == "pop":
+N = int(input())
+for _ in range(N):
+    command = list(input().split())
+    if command[0] == 'push':
+        stack.append(int(command[1]))
+    elif command[0] == 'pop':
         if len(stack) == 0:
             print(-1)
         else:
-            print(stack.pop())
-    elif command[:4] == "size":
+            num = stack.pop()
+            print(num)
+    elif command[0] == 'size':
         print(len(stack))
-    elif command[:5] == "empty":
+    elif command[0] == 'empty':
         if len(stack) == 0:
             print(1)
         else:
             print(0)
-    elif command[:3] == "top":
+    elif command[0] == 'top':
         if len(stack) == 0:
             print(-1)
         else:
             print(stack[-1])
-
