7c40c38acb9f682abc15ebbf2c93b39236af3e52
R-J-Woo <jewoo20000624@gmail.com>
2026-01-31 10:47:15 +0900

    [Silver V] Title: 두라무리 휴지, Time: 96 ms, Memory: 35252 KB -BaekjoonHub

--- 백준/Silver/25178. 두라무리 휴지/두라무리 휴지.py
+++ 백준/Silver/25178. 두라무리 휴지/두라무리 휴지.py
@@ -1,5 +1,3 @@
-from collections import Counter
-
 N = int(input())
 A = list(input())
 B = list(input())
@@ -7,37 +5,34 @@ B = list(input())
 str_A = []
 str_B = []
 
-A_ch = {}
-B_ch = {}
-
-for ch in ['a', 'e', 'i', 'o', 'u']:
-    A_ch[ch] = 0
-    B_ch[ch] = 0
+noun_A, noun_B = {}, {}
+for code in range(ord('a'), ord('z') + 1):
+    ch = chr(code)
+    noun_A[ch] = 0
+    noun_B[ch] = 0
 
 for i in range(N):
-    if A[i] in ('a', 'e', 'i', 'o', 'u'):
-        A_ch[A[i]] += 1
-    else:
+    if A[i] not in ('a', 'e', 'i', 'o', 'u'):
         str_A.append(A[i])
 
-    if B[i] in ('a', 'e', 'i', 'o', 'u'):
-        B_ch[B[i]] += 1
-    else:
+    if B[i] not in ('a', 'e', 'i', 'o', 'u'):
         str_B.append(B[i])
+
+    noun_A[A[i]] += 1
+    noun_B[B[i]] += 1
 
 flag = True
 
+# 모음을 제거한 문자열 동일
 if ''.join(str_A) != ''.join(str_B):
     flag = False
 
+# 첫 글자와 마지막 글자가 동일
 if A[0] != B[0] or A[-1] != B[-1]:
     flag = False
 
-for ch in ('a', 'e', 'i', 'o', 'u'):
-    if A_ch[ch] != B_ch[ch]:
-        flag = False
-
-if Counter(A) != Counter(B):
+# 한 단어를 재배열해 다른 단어를 생성 가능
+if noun_A != noun_B:
     flag = False
 
 print("YES" if flag else "NO")
