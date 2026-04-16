29b2a3ed3b3986fef8c19cffda90b5e6adfefcb3
R-J-Woo <jewoo20000624@gmail.com>
2026-04-16 10:37:56 +0900

    [Silver II] Title: 촌수계산, Time: 56 ms, Memory: 34936 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.py
+++ 백준/Silver/2644. 촌수계산/촌수계산.py
@@ -1,42 +1,33 @@
-# 아이디어: 너비 우선 탐색(BFS) 알고리즘 이용
-
 from collections import deque
-import sys
-input = sys.stdin.readline
+
+def bfs(graph, start, target, visited):
+    queue = deque()
+    queue.append((start, 0)) # 현재 노드, 촌수
+    visited[start] = True
+
+    while queue:
+        node, count = queue.popleft()
+        if node == target:
+            return count
+        
+        for v in graph[node]:
+            if not visited[v]:
+                queue.append((v, count + 1))
+                visited[v] = True
+
+    return -1
+
 
 n = int(input())
 a, b = map(int, input().split())
+
+graph = [[] for _ in range(n + 1)]
 m = int(input())
-graph = [[0] for _ in range(n + 1)]
 for _ in range(m):
     x, y = map(int, input().split())
-    if graph[x][0] == 0:
-        graph[x][0] = y
-    else:
-        graph[x].append(y)
+    graph[x].append(y)
+    graph[y].append(x)
 
-    if graph[y][0] == 0:
-        graph[y][0] = x
-    else:
-        graph[y].append(x)
-
-
-def bfs(graph, start, visited, result):
-    queue = deque([start])
-    visited[start] = True
-    result[start] = 0
-
-    # 1촌 관계의 사람들은 현재 촌수에서 +1을 해서 저장
-    while queue:
-        v = queue.popleft()
-        for i in graph[v]:
-            if not visited[i]:
-                queue.append(i)
-                visited[i] = True
-                result[i] = result[v] + 1
-
-
-result = [-1] * (n + 1)
 visited = [False] * (n + 1)
-bfs(graph, a, visited, result)
-print(result[b])
+result = bfs(graph, a, b, visited)
+print(result)
