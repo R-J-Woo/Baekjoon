8809b85f981387f8ba8300b87a967b78076f10ec
R-J-Woo <jewoo20000624@gmail.com>
2026-05-17 00:41:12 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.01 ms, Memory: 62.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[2];
+        answer[0] = 0;
+        answer[1] = 0;
+        
+        for (int num : num_list) {
+            if (num % 2 == 0) {
+                answer[0] += 1;
+            } else {
+                answer[1] += 1;
+            }
+        }
+        
+        return answer;
+    }
+}
